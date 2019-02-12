31a5dc12272c3d74835342dd90959371a7653037
uladzislau <uladzislau.zayats@gmail.com>
2019-02-12 17:54:30 +0100

    PTWO-217 exmg-sortable

--- test/exmg-sortable.spec.ts
+++ test/exmg-sortable.spec.ts
@@ -7,11 +7,9 @@ const {assert} = chai;
 suite('<exmg-sortable>', function () {
   let element: SortableElement;
   suite('element with children', function () {
-    setup(() => {
+    test('default sorting works properly', (done) => {
       element = fixture('ExmgSortableList');
-    });
 
-    test('element has list of element ', (done) => {
       element.addEventListener('dom-order-change', (event: Event) => {
         const detail = (<CustomEvent>event).detail;
         assert.equal(detail.sourceIndex, 0, 'Source index should be 0');
@@ -59,6 +57,60 @@ suite('<exmg-sortable>', function () {
 
       parent!.insertBefore(draggedElem, element.querySelectorAll('li')[2]);
       parent!.removeChild(element.querySelector('li.cloned')!);
+    });
+
+    test('custom handle sorting works properly', (done) => {
+      element = fixture('ExmgSortableListWithCustomHandle');
+
+      element.addEventListener('dom-order-change', (event: Event) => {
+        const detail = (<CustomEvent>event).detail;
+        assert.equal(detail.sourceIndex, 0, 'Source index should be 0');
+        assert.equal(detail.targetIndex, 1, 'Target index should be 1');
+        done();
+      });
+
+      const parent = element.querySelector('tbody');
+      const draggedElem = element.querySelectorAll('.handle span')[0];
+      const draggedElemClosestRow = draggedElem.closest('tr');
+
+      console.log('draggedElem', draggedElem);
+      const trackStartEvent = new CustomEvent(
+        'track',
+        {
+          detail: {
+            state: 'start',
+          },
+          bubbles: true,
+        }
+      );
+      draggedElem.dispatchEvent(trackStartEvent);
+
+      const trackMoveEvent = new CustomEvent(
+        'track',
+        {
+          detail: {
+            state: 'track',
+            dx: 0,
+            dy: 45,
+          },
+          bubbles: true,
+        }
+      );
+      draggedElem.dispatchEvent(trackMoveEvent);
+
+      const trackEndEvent = new CustomEvent(
+        'track',
+        {
+          detail: {
+            state: 'end',
+          },
+          bubbles: true,
+        }
+      );
+      draggedElem.dispatchEvent(trackEndEvent);
+
+      parent!.insertBefore(draggedElemClosestRow!, element.querySelectorAll('tr')[2]);
+      parent!.removeChild(element.querySelector('tr.cloned')!);
     });
   });
 
